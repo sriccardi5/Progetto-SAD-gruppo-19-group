f6383036a13c24ce15f37fa24ad7cf7a055256dd
Utente <Utente@LAPTOP-JB9TL4OL.mshome.net>
2025-05-19 18:08:59 +0200

    Task 2: ToolBar + Buttons

--- src/main/java/it/unisa/progettosadgruppo19/controller/PrimaryController.java
+++ src/main/java/it/unisa/progettosadgruppo19/controller/PrimaryController.java
@@ -4,18 +4,28 @@
  */
 package it.unisa.progettosadgruppo19.controller;
 
-
-
 import javafx.fxml.FXML;
+import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
 
 public class PrimaryController {
 
     @FXML private Pane drawingPane;
+    @FXML private Button lineButton;
+    @FXML private Button rectButton;
+    @FXML private Button ellipseButtjon;
+
+    private String selectedShape = "Linea"; // Valore di default
 
     @FXML
-    public void initialize() { 
-        drawingPane.getChildren().clear();    
+    public void initialize() {
+        drawingPane.getChildren().clear();   
+        
+        // Imposta la forma selezionata in base al bottone cliccato
+        lineButton.setOnAction(e -> selectedShape = "Linea");
+        rectButton.setOnAction(e -> selectedShape = "Rettangolo");
+        ellipseButton.setOnAction(e -> selectedShape = "Ellisse");
     }
+
 }
 
