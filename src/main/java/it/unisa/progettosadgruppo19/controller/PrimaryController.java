3f2c9b59ea49256a0c72e46298bd2249c5fc14c0
fpagl <fpagl@LAPTOP-TBFCMNKP>
2025-05-19 11:15:06 +0200

    Finestra vuota

---
+++ src/main/java/it/unisa/progettosadgruppo19/controller/PrimaryController.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package it.unisa.progettosadgruppo19.controller;
+
+
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+
+public class PrimaryController {
+
+    @FXML private Pane drawingPane;
+
+    @FXML
+    public void initialize() {
+        drawingPane.getChildren().clear();    
+    }
+}
+
